e16b2b5b053900e9a71da2f08dd5844d5262ea60
Quentin Millardet <quentin.millardet@telecomnancy.eu>
2020-10-18 14:12:36 +0200

    Add fake data at creation

--- iotlab/entrypoint.sh
+++ iotlab/entrypoint.sh
@@ -4,7 +4,7 @@ else
   sed  -i 's#<property name="jdbcUrl" value="jdbc:mysql://localhost:3306/iotlab"/>#<property name="jdbcUrl" value="jdbc:mysql://iotlab-db:3306/iotlab"/>#g' /usr/local/glassfish4/bin/bonecp-datasource.xml
   /usr/local/glassfish4/bin/asadmin add-resources /usr/local/glassfish4/bin/bonecp-datasource.xml
   mysql -h iotlab-db -uadmin -padmin iotlab < /home/iotlab/server/sql/iotlab.sql
-  cat /usr/local/glassfish4/bin/bonecp-datasource.xml
+  mysql -h iotlab-db -uadmin -padmin iotlab < /home/data.sql
   /usr/local/glassfish4/bin/asadmin deploy /home/iotlab/server/bin/iotlab.war
   touch /home/.installFinish
 fi
